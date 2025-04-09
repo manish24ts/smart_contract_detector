401b800cf3ffb275f8ba2a3f699b1817b7da65b5
T S Manish <126053896+manish24ts@users.noreply.github.com>
2025-04-09 22:15:46 +0530

    Update app.py
    changing the port for production

--- smart-contract-detector/app.py
+++ smart-contract-detector/app.py
@@ -59,4 +59,5 @@ def about():
     return render_template('./templates/about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Fallback to 10000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
